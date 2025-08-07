0ac418833797d52bb126b982cf26447aeced3962
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-07 14:38:14 +0900

    feat: add counter using class component and setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import { useState } from "react";
+import { Component } from "react";
 import "./App.css";
-
-function App() {
-  const [counter, setCounter] = useState(0);
-  return (
-    <>
-      <div>counter : {counter}</div>
-      <button
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setCounter((prev) => prev - 1);
-        }}
-      >
-        -
-      </button>
-    </>
-  );
+class App extends Component {
+  state = { counter: 0 };
+  render() {
+    return (
+      <>
+        <div>counter {this.state.counter}</div>
+        <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>+</button>
+        <button onClick={() => this.setState({ counter: this.state.counter - 1 })}>-</button>
+      </>
+    );
+  }
 }
 
 export default App;
