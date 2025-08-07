cbbafb4b72c6bd8392c3abb6b9a55e92fc6dd895
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-07 14:29:54 +0900

    feat: implement counter using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
   return (
     <>
       <div>counter : {counter}</div>
       <button
         onClick={() => {
-          counter += 1;
+          setCounter(counter + 1);
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          counter -= 1;
+          setCounter(counter - 1);
         }}
       >
         -
