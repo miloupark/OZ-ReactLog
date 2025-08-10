3bd879b0f0c2609f3e7e704c0620f6723b6b36fd
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-10 19:32:43 +0900

    refactor: lift state to parent in CounterFunction and pass handlers via props

--- src/_notes/CounterFunction.jsx
+++ src/_notes/CounterFunction.jsx
@@ -3,30 +3,40 @@ import "../../src/App.css";
 
 function CounterFunction() {
   const [counter, setCounter] = useState(0);
+
   return (
     <>
       <Count counter={counter} array={[1, 2, 3, "안녕하세요"]} hello={"hello"} />
-      <button
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setCounter((prev) => prev - 1);
-        }}
-      >
-        -
-      </button>
+      <PlusButton setCounter={setCounter} />
+      <MinusButton setCounter={setCounter} />
     </>
   );
 }
 
-// 부모 컴포넌트에서 자식 컴포넌트로 데이터를 전달할 수 있다 (props)
-// 자식 컴포넌트는 props를 구조분해 할당으로 받아 사용할 수 있다
-// 숫자, 문자열, 배열 등 다양한 타입의 값을 props로 전달할 수 있다
+function PlusButton({ setCounter }) {
+  return (
+    <button
+      onClick={() => {
+        setCounter((prev) => prev + 1);
+      }}
+    >
+      +
+    </button>
+  );
+}
+
+function MinusButton({ setCounter }) {
+  return (
+    <button
+      onClick={() => {
+        setCounter((prev) => prev - 1);
+      }}
+    >
+      -
+    </button>
+  );
+}
+
 function Count({ counter, array, hello }) {
   return <div>counter : {counter}</div>;
 }
