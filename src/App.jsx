27cd5f28b26ca1882139bf7beb45dc5e392e5f46
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-20 13:07:03 +0900

    feat(button): add prop-driven PropsButton

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,57 @@ function App() {
     <>
       <Container>
         <p>Styled-Components</p>
+        {/* BlueButton */}
         <BlueButton>BlueButton</BlueButton>
+        <PropsButton
+          backgroundColor="blue"
+          textColor="white"
+          padding="10px"
+          radius="8px"
+          margin="10px"
+        >
+          PropsButton
+        </PropsButton>
+
+        {/* LargeBlueButton */}
         <LargeBlueButton>LargeBlueButton</LargeBlueButton>
+        <PropsButton
+          backgroundColor="blue"
+          textColor="white"
+          padding="20px"
+          radius="8px"
+          margin="10px"
+          width="300px"
+        >
+          PropsButton
+        </PropsButton>
+
+        {/* LargeTextBlueButton */}
         <LargeTextBlueButton>LargeTextBlueButton</LargeTextBlueButton>
+        <PropsButton
+          backgroundColor="blue"
+          textColor="white"
+          padding="20px"
+          radius="8px"
+          margin="10px"
+          width="300px"
+          fontSize="24px"
+          fontWeight="800"
+        >
+          PropsButton
+        </PropsButton>
       </Container>
     </>
   );
 }
+
+// container
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
 
 // button
 const BlueButton = styled.button`
@@ -33,12 +77,16 @@ const LargeTextBlueButton = styled(LargeBlueButton)`
   font-weight: 800;
 `;
 
-// container
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: grid;
-  place-items: center;
+// props
+const PropsButton = styled.button`
+  width: ${(props) => props.width || "auto"};
+  font-size: ${(props) => props.fontSize || "auto"};
+  font-weight: ${(props) => props.fontWeight || 400};
+  background-color: ${(props) => props.backgroundColor || "white"};
+  color: ${(props) => props.textColor || "black"};
+  padding: ${(props) => props.padding || 0};
+  border-radius: ${(props) => props.radius || 0};
+  margin: ${(props) => props.margin || 0};
 `;
 
 export default App;
